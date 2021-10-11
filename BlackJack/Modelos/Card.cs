50690d1b1c5f93a13fb4ef2173713514fb4c9a77
Juan Sebastian Tarazona Quintero <juan.tarazona.2021@upb.edu.co>
2021-10-10 21:30:54 -0500

    Se creo la clase Card

---
+++ BlackJack/Modelos/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.Modelos
+{
+    class Card
+    {
+        public char Suit { get; set; }
+        public string Symbol { get; set; }
+
+        public int Score { get; set; }
+
+        public Color { get; set; }
+
+    }
+}
+
